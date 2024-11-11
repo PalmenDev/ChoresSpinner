b2d936cf60d21a67abac7a34bbe06ddbe92c4643
PalmenDev <simon.palm98@hotmail.se>
2024-11-11 19:04:27 +0100

    Update choresFactory

--- ChoresSpinner/src/components/ChoresFactory.jsx
+++ ChoresSpinner/src/components/ChoresFactory.jsx
@@ -1,34 +1,40 @@
-import {useState} from 'react'
+import {useRef, useState} from 'react'
 import SpinCircle from './Spinner';
+import ListOfChores from './ListOfChores';
 
 function ChoresFactory() {
-const [choreName, setChoresName] = useState('');
-let [chores, setChores] = useState([]);
 
-let choresId = 0;
+const [choreName, setChoresName] = useState('');
+const inputRef = useRef(null);
+let [chores, setChores] = useState([]);
 
 const clearAllChores = () => {
    setChores([]);
 }
 
+const clearInputValue = () => {
+    inputRef.current.value = '';
+}
+
+
   return (
     <div className='w-full mx-auto justify-center grid mt-12'>
-    <div className='flex'>
-            <input type='text' placeholder='Add chore' onChange={e => setChoresName(e.target.value)}/> 
+        <div className='flex'>
+            <input type='text' placeholder='Add chore' onChange={e => setChoresName(e.target.value)} ref={inputRef} /> 
             <button type='submit' className='w-32 p-2 bg-green-600' onClick={() => {
-                setChores([...chores, {id: choresId++, name: choreName, value: 20}])
-            }}> Add Chore </button>
-    </div>
-        <button className='mt-2 w-32 p-2 bg-red-600' onClick={clearAllChores}> Clear all chores </button>
-    <div className='mt-12 bg-slate-700 rounded-lg p-12'>
-        <ul>
-            {chores.map(chore => (
-                <li key={chore.id}>{chore.name}</li>
-            ))}
-        </ul>
-    </div>
+                setChores([...chores, {id: chores.length, name: choreName, value: 20}])
+                clearInputValue()
+                }}> Add Chore 
+            </button>
+        </div>
+        <button className='mt-2 w-32 p-2 bg-red-600' onClick={clearAllChores}> 
+            Clear all chores 
+        </button>
+
+        <ListOfChores listOfChores={chores}/>
         <SpinCircle chores={chores} />
-    </div>
+
+        </div>
   )
 }
 
